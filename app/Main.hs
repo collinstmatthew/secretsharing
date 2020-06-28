73ddd5d3d3843368f0d6a55945f6bce581bf7092
collinstmatthew <collins.t.matthew@gmail.com>
2020-06-28 23:00:46 +0100

    changed main to n=5

--- app/Main.hs
+++ app/Main.hs
@@ -13,13 +13,13 @@ import Prelude as P
 
 main :: IO()
 main = do
-  let mySecret = "Secret !? X"
+  let mySecret = "Secret SHHH"
   let v = Vault {threshold = 5, shares = 5, secret = mySecret, fieldOne = F.one :: Modp 7919}
 
-  --[s1,s2,s3] <- generateShare v
+  --[s1,s2,s3,s4,s5] <- generateShare v
   s <- generateShare v
   --result <- decrypt [s1,s2]
   let result = decrypt s
 
  -- print "results"
-  print result
+  print $ decrypt [s1,s2,s3,s4,s5]
